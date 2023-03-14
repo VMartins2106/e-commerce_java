4a4c04dfd8c30a433ec859ecd18bf063f4d6f732
Victor de Almeida Martins <victor.almeida.martins21@gmail.com>
2023-03-14 13:47:53 -0300

    E-commerce Java

---
+++ JFRAME/src/jframe/DBConnection.java
@@ -0,0 +1,18 @@
+package jframe;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+    static Connection con = null;
+    
+    public static Connection getConnection(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/e_commercejava_db","root","");
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return con;
+    }
+}
